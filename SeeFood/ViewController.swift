2b3eebe0c750aedbe19db79aa8364355e7349406
Chen Chen <chenchen@chens-macbook-pro.local>
2019-03-03 11:50:17 -0600

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,19 @@
+// 
+// SeeFood
+//
+// Created by Chen Chen on 3/3/19
+// Copyright © 2018 Timessis LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
